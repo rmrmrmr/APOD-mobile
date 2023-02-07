de9e449b2420106488a434d51bc438b7a0219323
rmrmrmr <ricardomartineza77@gmail.com>
2023-02-07 17:31:45 -0600

    Style searchbar and color alternation on cards

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -7,7 +7,7 @@ const Picture = ({
   pic, name, ind, date,
 }) => {
   return (
-    <div className={`flex w-1/2 h-48 flex-col m-0 px-2 py-2 ${[0, 3, 4, 6, 7, 8].includes(ind) ? 'bg-blue-500' : 'bg-blue-600'}`}>
+    <div className={`flex w-1/2 h-48 flex-col m-0 px-2 py-2 ${[0, 3, 4, 7, 8].includes(ind) ? 'bg-blue-500' : 'bg-blue-600'}`}>
       <img src={pic} alt="Img not found" className="h-2/3 w-full rounded-md mb-2" />
       <h2 className="text-white font-semibold text-xs text-right pl-2">{name}</h2>
       <h3 className="text-white font-medium text-xs text-right">{date}</h3>
